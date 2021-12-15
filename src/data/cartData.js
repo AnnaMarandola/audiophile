876a89085afd64ec66ec908156002d2720bac630
AnnaMarandola <annamarandola@gmail.com>
2021-12-15 23:09:49 +0100

    adding spearkers data

--- src/data/cartData.js
+++ src/data/cartData.js
@@ -1,12 +1,14 @@
 import CARTXX991 from "../assets/cart/image-xx99-mark-one-headphones.jpg";
 import CARTXX992 from "../assets/cart/image-xx99-mark-two-headphones.jpg";
 import CARTXX59 from "../assets/cart/image-xx59-headphones.jpg";
-
+import CARTZX7 from "../assets/cart/image-zx7-speaker.jpg";
+import CARTZX9 from "../assets/cart/image-zx9-speaker.jpg";
 const cartData = [
   { id: "XX99 Mark II Headphones", name:"XX99 Mark II", picture: CARTXX992, price: "2,999" },
   { id: "XX99 Mark I Headphones", name:"XX99 Mark I", picture: CARTXX991, price: "1,750" },
   { id: "XX59 Headphones", name:"XX59", picture: CARTXX59, price: "8,99" },
-  { name: "", picture: "", price: 12 },
+  { id: "ZX7 Speaker", name: "ZX7 Speaker", picture: CARTZX7, price: "3,500", },
+  { id: "ZX9 Speaker", name: "ZX9 Speaker", picture: CARTZX9, price: "4,500", },
   { name: "", picture: "", price: 12 },
   { name: "", picture: "", price: 12 },
   { name: "", picture: "", price: 12 },
--- src/data/productsData.js
+++ src/data/productsData.js
@@ -19,6 +19,10 @@ import OTHERXX59D from "../assets/shared/desktop/image-xx59-headphones.jpg";
 import OTHERZX9M from "../assets/shared/mobile/image-zx9-speaker.jpg";
 import OTHERZX9T from "../assets/shared/tablet/image-zx9-speaker.jpg";
 import OTHERZX9D from "../assets/shared/desktop/image-zx9-speaker.jpg";
+import OTHERZX7M from "../assets/shared/mobile/image-zx7-speaker.jpg";
+import OTHERZX7T from "../assets/shared/tablet/image-zx7-speaker.jpg";
+import OTHERZX7D from "../assets/shared/desktop/image-zx7-speaker.jpg";
+
 
 import XX99GALLERY1M from "../assets/product-xx99-mark-one-headphones/mobile/image-gallery-1.jpg";
 import XX99GALLERY2M from "../assets/product-xx99-mark-one-headphones/mobile/image-gallery-2.jpg";
@@ -29,7 +33,28 @@ import XX99GALLERY3T from "../assets/product-xx99-mark-one-headphones/tablet/ima
 import XX99GALLERY1D from "../assets/product-xx99-mark-one-headphones/desktop/image-gallery-1.jpg";
 import XX99GALLERY2D from "../assets/product-xx99-mark-one-headphones/desktop/image-gallery-2.jpg";
 import XX99GALLERY3D from "../assets/product-xx99-mark-one-headphones/desktop/image-gallery-3.jpg";
+//speakers
+import ZX7PREVIEW from "../assets/product-zx7-speaker/mobile/image-category-page-preview.jpg";
+import ZX7GALLERY1M from "../assets/product-zx7-speaker/mobile/image-gallery-1.jpg";
+import ZX7GALLERY2M from "../assets/product-zx7-speaker/mobile/image-gallery-2.jpg";
+import ZX7GALLERY3M from "../assets/product-zx7-speaker/mobile/image-gallery-3.jpg";
+import ZX7GALLERY1T from "../assets/product-zx7-speaker/tablet/image-gallery-1.jpg";
+import ZX7GALLERY2T from "../assets/product-zx7-speaker/tablet/image-gallery-2.jpg";
+import ZX7GALLERY3T from "../assets/product-zx7-speaker/tablet/image-gallery-3.jpg";
+import ZX7GALLERY1D from "../assets/product-zx7-speaker/desktop/image-gallery-1.jpg";
+import ZX7GALLERY2D from "../assets/product-zx7-speaker/desktop/image-gallery-2.jpg";
+import ZX7GALLERY3D from "../assets/product-zx7-speaker/desktop/image-gallery-3.jpg";
 
+import ZX9PREVIEW from "../assets/product-zx9-speaker/mobile/image-category-page-preview.jpg";
+import ZX9GALLERY1M from "../assets/product-zx9-speaker/mobile/image-gallery-1.jpg";
+import ZX9GALLERY2M from "../assets/product-zx9-speaker/mobile/image-gallery-2.jpg";
+import ZX9GALLERY3M from "../assets/product-zx9-speaker/mobile/image-gallery-3.jpg";
+import ZX9GALLERY1T from "../assets/product-zx9-speaker/tablet/image-gallery-1.jpg";
+import ZX9GALLERY2T from "../assets/product-zx9-speaker/tablet/image-gallery-2.jpg";
+import ZX9GALLERY3T from "../assets/product-zx9-speaker/tablet/image-gallery-3.jpg";
+import ZX9GALLERY1D from "../assets/product-zx9-speaker/desktop/image-gallery-1.jpg";
+import ZX9GALLERY2D from "../assets/product-zx9-speaker/desktop/image-gallery-2.jpg";
+import ZX9GALLERY3D from "../assets/product-zx9-speaker/desktop/image-gallery-3.jpg";
 
 const productsData = [
   {
@@ -200,8 +225,8 @@ const productsData = [
             },
           },
         ],
-
       },
+
       {
         id: 3,
         slug: "xx59-headphones",
@@ -295,15 +320,259 @@ const productsData = [
             },
           },
         ],
-
       },
     ],
   },
+
   {
     category: "speakers",
-    products: [],
+    products: [
+      {
+        id: 4,
+        slug: "zx7-speaker",
+        name: "ZX7 Speaker",
+        categoryImage: ZX7PREVIEW,
+        category: "speakers",
+        new: false,
+        price: "3,500",
+        description:
+          "Stream high quality sound wirelessly with minimal to no loss. The ZX7 speaker uses high-end audiophile components that represents the top of the line powered speakers for home or studio use.",
+        features:
+          "Reap the advantages of a flat diaphragm tweeter cone. This provides a fast response rate and excellent high frequencies that lower tiered bookshelf speakers cannot provide. The woofers are made from aluminum that produces a unique and clear sound. XLR inputs allow you to connect to a mixer for more advanced usage.\n\nThe ZX7 speaker is the perfect blend of stylish design and high performance. It houses an encased MDF wooden enclosure which minimises acoustic resonance. Dual connectivity allows pairing through bluetooth or traditional optical and RCA input. Switch input sources and control volume at your finger tips with the included wireless remote. This versatile speaker is equipped to deliver an authentic listening experience.",
+        includes: [
+          {
+            quantity: 2,
+            item: "Speaker unit",
+          },
+          {
+            quantity: 2,
+            item: "Speaker cloth panel",
+          },
+          {
+            quantity: 1,
+            item: "User manual",
+          },
+          {
+            quantity: 1,
+            item: "3.5mm 7.5m audio cable",
+          },
+          {
+            quantity: 1,
+            item: "7.5m optical cable",
+          },
+        ],
+        gallery: {
+          first: {
+            mobile: ZX7GALLERY1M,
+            tablet: ZX7GALLERY1T,
+            desktop: ZX7GALLERY1D,
+          },
+          second: {
+            mobile: ZX7GALLERY2M,
+            tablet: ZX7GALLERY2T,
+            desktop: ZX7GALLERY2D,
+          },
+          third: {
+            mobile: ZX7GALLERY3M,
+            tablet: ZX7GALLERY3T,
+            desktop: ZX7GALLERY3D,
+          },
+        },
+        others: [
+          {
+            slug: "zx9-speaker",
+            name: "ZX9 Speaker",
+            category: "speakers",
+            image: {
+              mobile: OTHERZX9M,
+              tablet: OTHERZX9T,
+              desktop: OTHERZX9D,
+            },
+          },
+          {
+            slug: "xx99-mark-one-headphones",
+            name: "XX99 Mark I",
+            category: "headphones",
+
+            image: {
+              mobile: OTHERXX99M,
+              tablet: OTHERXX99T,
+              desktop: OTHERXX99D,
+            },
+          },
+          {
+            slug: "xx59-headphones",
+            name: "XX59",
+            category: "headphones",
+
+            image: {
+              mobile: OTHERXX59M,
+              tablet: OTHERXX59T,
+              desktop: OTHERXX59D,
+            },
+          },
+        ],
+      },
+      {
+        id: 5,
+        slug: "zx9-speaker",
+        name: "ZX9 Speaker",
+        categoryImage: ZX9PREVIEW,
+        category: "speakers",
+        new: true,
+        price: "4,500",
+        description:
+          "Upgrade your sound system with the all new ZX9 active speaker. It’s a bookshelf speaker system that offers truly wireless connectivity -- creating new possibilities for more pleasing and practical audio setups.",
+        features:
+          "Connect via Bluetooth or nearly any wired source. This speaker features optical, digital coaxial, USB Type-B, stereo RCA, and stereo XLR inputs, allowing you to have up to five wired source devices connected for easy switching. Improved bluetooth technology offers near lossless audio quality at up to 328ft (100m).\n\nDiscover clear, more natural sounding highs than the competition with ZX9’s signature planar diaphragm tweeter. Equally important is its powerful room-shaking bass courtesy of a 6.5” aluminum alloy bass unit. You’ll be able to enjoy equal sound quality whether in a large room or small den. Furthermore, you will experience new sensations from old songs since it can respond to even the subtle waveforms.",
+        includes: [
+          {
+            quantity: 2,
+            item: "Speaker unit",
+          },
+          {
+            quantity: 2,
+            item: "Speaker cloth panel",
+          },
+          {
+            quantity: 1,
+            item: "User manual",
+          },
+          {
+            quantity: 1,
+            item: "3.5mm 10m audio cable",
+          },
+          {
+            quantity: 1,
+            item: "10m optical cable",
+          },
+        ],
+        gallery: {
+          first: {
+            mobile: ZX9GALLERY1M,
+            tablet: ZX9GALLERY1T,
+            desktop: ZX9GALLERY1D,
+          },
+          second: {
+            mobile: ZX9GALLERY2M,
+            tablet: ZX9GALLERY2T,
+            desktop: ZX9GALLERY2D,
+          },
+          third: {
+            mobile: ZX9GALLERY3M,
+            tablet: ZX9GALLERY3T,
+            desktop: ZX9GALLERY3D,
+          },
+        },
+        others: [
+          {
+            slug: "xx99-mark-one-headphones",
+            name: "XX99 Mark I",
+            category: "headphones",
+
+            image: {
+              mobile: OTHERXX99M,
+              tablet: OTHERXX99T,
+              desktop: OTHERXX99D,
+            },
+          },
+          {
+            slug: "xx59-headphones",
+            name: "XX59",
+            category: "headphones",
+
+            image: {
+              mobile: OTHERXX59M,
+              tablet: OTHERXX59T,
+              desktop: OTHERXX59D,
+            },
+          },
+          {
+            slug: "zx7-speaker",
+            name: "ZX7 Speaker",
+            image: {
+              mobile: OTHERZX7M,
+              tablet: OTHERZX7T,
+              desktop: OTHERZX7D,
+            },
+          },
+        ],
+      },
+    ],
   },
-  { category: "earphones", products: [] },
+
+
+  { category: "earphones",
+ products: [
+  {
+    id: 5,
+    slug: "",
+    name: "",
+    categoryImage: "",
+    category: "speakers",
+    new: true,
+    price: "",
+    description: "",
+    features: "",
+    includes: [
+      {
+        quantity: 1,
+        item: "",
+      },
+    ],
+    gallery: {
+      first: {
+        mobile: XX992GALLERY1M,
+        tablet: XX992GALLERY1T,
+        desktop: XX992GALLERY1D,
+      },
+      second: {
+        mobile: XX992GALLERY2M,
+        tablet: XX992GALLERY2T,
+        desktop: XX992GALLERY2D,
+      },
+      third: {
+        mobile: XX992GALLERY3M,
+        tablet: XX992GALLERY3T,
+        desktop: XX992GALLERY3D,
+      },
+    },
+    others: [
+      {
+        slug: "xx99-mark-one-headphones",
+        name: "XX99 Mark I",
+        category: "headphones",
+
+        image: {
+          mobile: OTHERXX99M,
+          tablet: OTHERXX99T,
+          desktop: OTHERXX99D,
+        },
+      },
+      {
+        slug: "xx59-headphones",
+        name: "XX59",
+        category: "headphones",
+
+        image: {
+          mobile: OTHERXX59M,
+          tablet: OTHERXX59T,
+          desktop: OTHERXX59D,
+        },
+      },
+      {
+        slug: "zx9-speaker",
+        name: "ZX9 Speaker",
+        image: {
+          mobile: OTHERZX9M,
+          tablet: OTHERZX9T,
+          desktop: OTHERZX9D,
+        },
+      },
+    ],
+  },
+
+ ]}
 ];
 
 export default productsData;
